617d61fe98e785ce30d13bd46a799dc5ab4e5ce5
Laurence de Jong <info@ldej.nl>
2023-06-01 16:22:27 +0530

    Clean up naming in oapi-codegen server

--- oapi-codegen/server.go
+++ oapi-codegen/server.go
@@ -17,20 +17,28 @@ import (
 )
 
 func main() {
+	// Create your usual storage/service/handler layers
+	store := helpers.NewThingsDatastore()
+	service := NewThingService(store)
+	s := NewServer(service)
+
 	swagger, err := api.GetSwagger()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r := chi.NewRouter()
-	r.Get("/swagger/doc.json", func(w http.ResponseWriter, r *http.Request) {
+	router := chi.NewRouter()
+
+	// Add swagger UI endpoints
+	router.Get("/swagger/doc.json", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(swagger)
 	})
-	r.Handle("/swagger/", middleware.SwaggerUI(middleware.SwaggerUIOpts{
+	router.Handle("/swagger/", middleware.SwaggerUI(middleware.SwaggerUIOpts{
 		Path:    "/swagger/",
 		SpecURL: "/swagger/doc.json",
 	}, nil))
 
+	// Enable validation of incoming requests
 	validator := chimiddleware.OapiRequestValidatorWithOptions(
 		swagger,
 		&chimiddleware.Options{
@@ -42,16 +50,11 @@ func main() {
 		},
 	)
 
-	// Create your usual storage/service/handler layers
-	thingsStore := helpers.NewThingsDatastore()
-	thingsHandler := NewThingsHandler(thingsStore)
-	s := NewServer(thingsHandler)
-
 	apiServer := api.HandlerWithOptions(
 		api.NewStrictHandler(s, nil),
 		api.ChiServerOptions{
 			BaseURL:     "/api/v1",
-			BaseRouter:  r,
+			BaseRouter:  router,
 			Middlewares: []api.MiddlewareFunc{validator},
 		},
 	)
@@ -70,27 +73,27 @@ func main() {
 	}
 }
 
-func NewServer(thingsHandler *ThingsHandler) api.StrictServerInterface {
+func NewServer(thingService *ThingService) api.StrictServerInterface {
 	return &server{
-		ThingsHandler: thingsHandler,
+		ThingService: thingService,
 	}
 }
 
 type server struct {
-	*ThingsHandler
+	*ThingService
 }
 
-type ThingsHandler struct {
+type ThingService struct {
 	store *helpers.ThingsDatastore
 }
 
-func NewThingsHandler(store *helpers.ThingsDatastore) *ThingsHandler {
-	return &ThingsHandler{
+func NewThingService(store *helpers.ThingsDatastore) *ThingService {
+	return &ThingService{
 		store: store,
 	}
 }
 
-func (h *ThingsHandler) CreateThing(ctx context.Context, request api.CreateThingRequestObject) (api.CreateThingResponseObject, error) {
+func (s *ThingService) CreateThing(ctx context.Context, request api.CreateThingRequestObject) (api.CreateThingResponseObject, error) {
 	thing := helpers.Thing{
 		UUID:    uuid.NewString(),
 		Name:    request.Body.Name,
@@ -100,15 +103,15 @@ func (h *ThingsHandler) CreateThing(ctx context.Context, request api.CreateThing
 		Type:    string(request.Body.Type),
 		Created: time.Now().UTC(),
 	}
-	err := h.store.StoreThing(thing)
+	err := s.store.StoreThing(thing)
 	if err != nil {
 		return api.CreateThingdefaultJSONResponse{Body: api.Error{Message: err.Error()}, StatusCode: http.StatusInternalServerError}, nil
 	}
 	return api.CreateThing200JSONResponse(mapThingToThingResponse(thing)), nil
 }
 
-func (h *ThingsHandler) DeleteThing(ctx context.Context, request api.DeleteThingRequestObject) (api.DeleteThingResponseObject, error) {
-	err := h.store.DeleteThing(request.Uuid.String())
+func (s *ThingService) DeleteThing(ctx context.Context, request api.DeleteThingRequestObject) (api.DeleteThingResponseObject, error) {
+	err := s.store.DeleteThing(request.Uuid.String())
 	if err == helpers.ErrNotFound {
 		return api.DeleteThing404JSONResponse{}, nil
 	}
@@ -118,8 +121,8 @@ func (h *ThingsHandler) DeleteThing(ctx context.Context, request api.DeleteThing
 	return api.DeleteThing204Response{}, nil
 }
 
-func (h *ThingsHandler) GetThing(ctx context.Context, request api.GetThingRequestObject) (api.GetThingResponseObject, error) {
-	thing, err := h.store.GetThing(request.Uuid.String())
+func (s *ThingService) GetThing(ctx context.Context, request api.GetThingRequestObject) (api.GetThingResponseObject, error) {
+	thing, err := s.store.GetThing(request.Uuid.String())
 	if err == helpers.ErrNotFound {
 		return api.GetThing404JSONResponse{}, nil
 	}
@@ -129,8 +132,8 @@ func (h *ThingsHandler) GetThing(ctx context.Context, request api.GetThingReques
 	return api.GetThing200JSONResponse(mapThingToThingResponse(thing)), nil
 }
 
-func (h *ThingsHandler) UpdateThing(ctx context.Context, request api.UpdateThingRequestObject) (api.UpdateThingResponseObject, error) {
-	err := h.store.UpdateThing(helpers.Thing{
+func (s *ThingService) UpdateThing(ctx context.Context, request api.UpdateThingRequestObject) (api.UpdateThingResponseObject, error) {
+	err := s.store.UpdateThing(helpers.Thing{
 		UUID:  request.Uuid.String(),
 		Score: request.Body.Score,
 	})
@@ -143,13 +146,13 @@ func (h *ThingsHandler) UpdateThing(ctx context.Context, request api.UpdateThing
 	return api.UpdateThing204Response{}, nil
 }
 
-func (h *ThingsHandler) ListThings(ctx context.Context, request api.ListThingsRequestObject) (api.ListThingsResponseObject, error) {
+func (s *ThingService) ListThings(ctx context.Context, request api.ListThingsRequestObject) (api.ListThingsResponseObject, error) {
 	limit := 10
 	offset := 0
 	if request.Params.Page != nil {
 		offset = *request.Params.Page * limit
 	}
-	things, total := h.store.ListThings(offset, limit)
+	things, total := s.store.ListThings(offset, limit)
 	response := api.ListThings200JSONResponse{Total: total, Things: []api.ThingResponse{}}
 	for _, thing := range things {
 		response.Things = append(response.Things, mapThingToThingResponse(thing))
